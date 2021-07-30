508675913150df24eb50ca10db2fa32980945152
William Shepherd <iAmWillShepherd@users.noreply.github.com>
2021-07-30 18:03:05 -0500

    Declare a type

--- src/t/index.d.ts
+++ src/t/index.d.ts
@@ -1,7 +1,3 @@
 declare module 'lodash' {
-  function chunk(array: string[], size?: number): string[]
-
-  function chunk(array: number[], size?: number): number[]
-
-  function chunk(array: boolean[], size?: number): boolean[]
+  function chunk<T>(array: T[], size?: number): T[]
 }
