235a522b6303392600be8d0057876cc38c2fc98d
Rodrigo <rogomezpalacio@protonmail.com>
2021-08-05 19:12:59 -0500

    Remove `t` directory in favor of `typeDeclarations` dir

---
+++ src/typeDeclarations/index.d.ts
@@ -0,0 +1,7 @@
+
+declare module 'lodash' {
+  //function chunk<T>(array: T[], size?: number): T[];
+  function chunk<T>(array: T[], size?: number): T[];
+  // function uniq<T extends Bird | Mammal>(animals: Array<T>): Array<T>;
+  //function filter<S extends T>(predicate:(animals: Array<T>) => animals is S[], callback: Function): S[];
+}
