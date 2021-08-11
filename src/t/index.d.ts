9655b2fb5d5093e1f8e12b9b9de27e835f0d289d
William Shepherd <iAmWillShepherd@users.noreply.github.com>
2021-08-11 05:23:50 -0500

    Remove types added by accident

--- src/t/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'lodash' {
-  function chunk(array: string[], size?: number): string[]
-
-  function chunk(array: number[], size?: number): number[]
-
-  function chunk(array: boolean[], size?: number): boolean[]
-}
