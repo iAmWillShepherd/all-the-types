8fc530dc3728372c7acd6eb65e7eadc280981f76
Rodrigo <rogomezpalacio@protonmail.com>
2021-08-16 19:07:18 -0500

    Update all-the-types file

--- src/commands/all-the-types.ts
+++ src/commands/all-the-types.ts
@@ -9,7 +9,7 @@ const command: GluegunCommand = {
     console.log("Generating animals");
 
     let animals: Animal[] = [];
-    for(let i=0; i<100; i++) {
+    for(let i = 0; i < 20; i++) {
       animals.push(RandomAnimal.get());
     }
 
@@ -41,21 +41,19 @@ const command: GluegunCommand = {
       }
     })
 
-    console.log('\n');
+    console.log('\n\n');
     console.log(allBirds.length, "total birds.")
     console.log(allMammals.length, "total mammals.")
     console.log(allFish.length, "total fish.")
     console.log(allReptiles.length, "total reptiles.")
-    console.log('\n');
 
     const birdCages = _.chunk<any>(allBirds, 3);
-    const mammalCages = _.chunk<any>(allMammals, 3);
 
+    console.log('\n\n');
     console.log(birdCages.length, "bird cages.")
-    console.log(mammalCages.length, "mammal cages.")
-    console.log('\n');
 
     // print all bird wingspans
+    console.log('\n\n');
     const birdsWithWingspans = _.partition(allBirds, (bird: any) => (bird.wingspan !== undefined));
     const wingspans = _.map(birdsWithWingspans[0], (bird: any) => bird.wingspan);
     console.log("wingspans:", wingspans);
@@ -71,18 +69,51 @@ const command: GluegunCommand = {
     // classify birds by size
     const smallBirds = _.filter(allBirds, (bird: any) => (bird.wingspan < 5));
     const mediumBirds = _.filter(allBirds, (bird: any) => (bird.wingspan < 10 && bird.wingspan >= 5));
-    const largeBirds = _.filter(allBirds, (bird: any) => (bird.wingspan < 20 && bird.wingspan >= 10));
+    const largeBirds = _.filter(allBirds, (bird: any) => (bird.wingspan <= 20 && bird.wingspan >= 10));
 
+    console.log('\n\n');
     console.log("small birds:", smallBirds);
     console.log("medium birds:", mediumBirds);
     console.log("large birds:", largeBirds);
 
     // print all animals of colors ___ (white, black, etc...)
-    console.log(animals);
+    console.log('\n\n');
     const animalGroupsByColor = _.groupBy(animals, 'color');
     console.log("animals by color:", animalGroupsByColor);
 
-    // print all animal species
+    // partition mammals by locomotion
+    const mammalsPartitionedByLocomotion = _.partition(allMammals, (mammal: any) => (mammal.locomotion === 'terrestrial'));
+    console.log('\n\n');
+    const terrestrialMammals = mammalsPartitionedByLocomotion[0];
+    const aquaticMammals = mammalsPartitionedByLocomotion[1];
+    console.log("terrestrial mammals:", terrestrialMammals);
+    console.log("aquatic mammals:", aquaticMammals);
+
+    // get reptiles with legs
+    console.log('\n\n');
+    const reptilesWithLegs = _.filter(allReptiles, (reptile: any) => (reptile.legs));
+    console.log("reptiles with legs:", reptilesWithLegs);
+
+    // get first reptile with legs
+    const firstReptileWithLegs = _.take(reptilesWithLegs);
+    console.log("first reptile with legs:", firstReptileWithLegs);
+
+    /* COMPLETE THE REST OF THESE TASKS */
+
+    // organize all the reptiles with legs by color
+
+    // separate the fish by water type
+
+    // create three size categories for the fish
+
+    // get all the reptiles with fangs
+
+    // separate the birds by ability to fly
+
+    // separate the birds by ability to swim
+
+    // get the first two reptiles with teeth
+
   }
 }
 
